0da073926afede20be56d15d52c4c3fd569c7545
CircuitBreaker88 <sarris88p@gmail.com>
2019-08-06 21:02:15 -0400

    NXTG rebrand to NXTL

--- src/currencies.js
+++ src/currencies.js
@@ -33,8 +33,8 @@
             symbol: 'dnr',
             addressTypes: {prod: ['1e', '5a'], testnet: ['12', '74']}
         },{
-          name: 'nxtlvlgreen',
-          symbol: 'nxtg',
+          name: 'nxtlvl',
+          symbol: 'nxtl',
           addressTypes: {prod: ['3F', '12'], testnet: ['8C', '13']}
         },{
             name: 'beavercoin',
